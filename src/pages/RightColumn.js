6343d59a8538c2644126162d3626c3ea4e161710
vinayreddy <vinaykumaramireddy@gmail.com>
2024-03-06 23:57:11 +0530

    new commit

--- src/pages/RightColumn.js
+++ src/pages/RightColumn.js
@@ -10,22 +10,22 @@ function RightColumn() {
         <h3>Financial News</h3>
         <ul>
           <li>
-            <a href="#">Stock Market Hits All-Time High</a>
+            <a >Stock Market Hits All-Time High</a>
           </li>
           <li>
-            <a href="#">Bitcoin Surpasses $50,000 Mark</a>
+            <a >Bitcoin Surpasses $50,000 Mark</a>
           </li>
           <li>
-            <a href="#">Federal Reserve Announces Interest Rate Decision</a>
+            <a >Federal Reserve Announces Interest Rate Decision</a>
           </li>
           <li>
-            <a href="#">Tech Giants Face Antitrust Scrutiny</a>
+            <a >Tech Giants Face Antitrust Scrutiny</a>
           </li>
           <li>
-            <a href="#">Economic Stimulus Package Passed by Congress</a>
+            <a >Economic Stimulus Package Passed by Congress</a>
           </li>
           <li>
-            <a href="#">Tesla Reports Record Vehicle Deliveries</a>
+            <a >Tesla Reports Record Vehicle Deliveries</a>
           </li>
         </ul>
       </div>
